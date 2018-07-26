54178610ae0f12e5e15746b35a692f0d0dfbc7d2
Raphael Lima <rbf.lima91@gmail.com>
2018-07-26 23:17:57 +0000

    Block.java edited online with Bitbucket

--- Block.java
+++ Block.java
@@ -6,7 +6,7 @@ public class Block {
 	public String hash;
 	public String previousHash; 
 	public String merkleRoot;
-	public ArrayList<Transaction> transactions = new ArrayList<Transaction>(); //our data will be a simple message.
+	public ArrayList<Transaction> transactions = new ArrayList<Transaction>(); 
 	public long timeStamp; //as number of milliseconds since 1/1/1970.
 	public int nonce;
 	
@@ -44,7 +44,7 @@ public class Block {
 	public boolean addTransaction(Transaction transaction) {
 		//process transaction and check if valid, unless block is genesis block then ignore.
 		if(transaction == null) return false;		
-		if((!"0".equals(previousHash))) {
+		if((!"0".equals(previousHash)) && transaction.data.isPayment) {
 			if((transaction.processTransaction() != true)) {
 				System.out.println("Transaction failed to process. Discarded.");
 				return false;
