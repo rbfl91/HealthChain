f83f9de64a00a8b9755ce4d5c64de836d3229d63
raphaellima <raphaellima@172.17.7.55>
2018-08-07 21:45:13 +0100

    Patient and Doctor classes

--- HealthString/src/Main.java
+++ HealthString/src/Main.java
@@ -10,7 +10,10 @@ import java.util.Map;
 public class Main {  
 	
 	// Unique list of UTXOs that will be used for the payment blockchain
-	public static HashMap<String,TransactionOutput> UTXOs = new HashMap<String,TransactionOutput>();
+	public static HashMap<String,TransactionOutput> UTXOs = new HashMap<String,TransactionOutput>(); 
+	
+	public static HealthChainPay chainPay = new HealthChainPay(); 
+	public static HealthChainData chainData = new HealthChainData();
 	
 	// Wallets (used for both blockchains)
 	public static Wallet walletA;
@@ -22,11 +25,39 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub  
 		
-		HealthChainPay chainPay = new HealthChainPay(); 
-		HealthChainData chainData = new HealthChainData();
-		
-		
 		Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider()); //Setup Bouncey castle as a Security Provider
+				
+		Patient patient1 = new Patient ("Pedro"); 
+		Doctor doctor1 = new Doctor ("Douglas");  
+		Doctor doctor2 = new Doctor ("Julio");
+		
+		Wallet coinbase = new Wallet();
+		
+		//create genesis transaction, which sends 100 coins to Pedro's wallet: 
+		genesisTransaction = new Transaction(coinbase.publicKey, patient1.getWallet().publicKey, "100", true, null);
+		genesisTransaction.generateSignature(coinbase.privateKey);	 //manually sign the genesis transaction	
+		genesisTransaction.transactionId = "0"; //manually set the transaction id
+		genesisTransaction.outputs.add(new TransactionOutput(genesisTransaction.reciepient, genesisTransaction.value, genesisTransaction.transactionId)); //manually add the Transactions Output
+		UTXOs.put(genesisTransaction.outputs.get(0).id, genesisTransaction.outputs.get(0)); //its important to store our first transaction in the UTXOs list.
+		
+		System.out.println("Creating and Mining Genesis block... ");
+		Block genesis = new Block("0");
+		genesis.addTransaction(genesisTransaction);
+		chainPay.addBlock(genesis); 
+		
+		patient1.makePayment(doctor1, 50); 
+		patient1.makePayment(doctor2, 40);
+		
+		
+		chainPay.getDataFrom(patient1.getWallet().publicKey, doctor1.getWallet().publicKey);
+		chainPay.isChainValid(); 
+		
+		/*
+		chainData.getDataFrom(walletB.publicKey, walletA.publicKey);
+		chainData.isChainValid();   
+		*/
+				
+		/*
 				
 				//Create wallets:
 				walletA = new Wallet();
@@ -77,15 +108,9 @@ public class Main {
 				Block block5 = new Block(block4.hash);
 				System.out.println("\nWalletB is Attempting to send data to WalletA...");
 				block5.addTransaction(walletB.sendData(walletA.publicKey, "Medical Data from Wallet B[2]")); 
-				chainData.addBlock(block5); 
+				chainData.addBlock(block5);  
 				
-				
-				chainPay.getDataFrom(walletB.publicKey, walletA.publicKey);
-				chainPay.isChainValid(); 
-				
-				
-				chainData.getDataFrom(walletB.publicKey, walletA.publicKey);
-				chainData.isChainValid(); 
+				*/
 				
 
 	}
